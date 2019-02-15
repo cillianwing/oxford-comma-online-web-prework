05f649ddcff3d3907839baa8cfbc0373426615e8
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-15 18:23:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   elsif array.length >= 3
-    final_array = (array[0]..array[array.length - 1]).join(", ")
-    final_array << ", and #{array[array.length"
+    final_array = (array[0]..array[array.length - 2]).join(", ")
+    final_array << ", and #{array[array.length - 1}"
+    final array
 end
